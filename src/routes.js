f05672483ae2b3ddee1f7accab6fff527b7d8a1a
Nastia Kartseva <danetzachem@gmail.com>
2022-05-13 18:26:08 +0300

    add browser routing

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ const host = '';
 const prefix = 'api/v1';
 
 export default {
+  loginPath: () => [host, prefix, 'login'].join('/'),
+  signupPath: () => [host, prefix, 'signup'].join('/'),
+  usersPath: () => [host, prefix, 'data'].join('/'),
   channelsPath: () => [host, prefix, 'channels'].join('/'),
   channelPath: (id) => [host, prefix, 'channels', id].join('/'),
   channelMessagesPath: (id) => [host, prefix, 'channels', id, 'messages'].join('/'),
