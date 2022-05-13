0edecd3d90820ea035d122cd39b24c278e388851
Nastia Kartseva <danetzachem@gmail.com>
2022-05-13 18:26:57 +0300

    update routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,29 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from 'react-router-dom';
 
+import { AuthProvider } from '../contexts/auth.jsx';
 import LoginForm from './LoginForm.jsx';
 import NoMatch from './NoMatch.jsx';
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route path="/login">
-        <LoginForm />
-      </Route>
-      <Route path="/" />
-      <Route path="*">
-        <NoMatch />
-      </Route>
-    </Switch>
-  </Router>
+  <AuthProvider>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginForm />
+        </Route>
+        <Route path="/">
+          { localStorage.getItem('userId') ? null : <Redirect to="/login" /> }
+        </Route>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
+  </AuthProvider>
 ); // нужен ли exact path у '/' ?
 
 export default App;
