0877dbbbfe16c7ec505608ad863d4557b83a9174
Nastia Kartseva <danetzachem@gmail.com>
2022-05-16 19:37:36 +0300

    edit routes

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const prefix = 'api/v1';
 export default {
   loginPath: () => [host, prefix, 'login'].join('/'),
   signupPath: () => [host, prefix, 'signup'].join('/'),
-  usersPath: () => [host, prefix, 'data'].join('/'),
+  dataPath: () => [host, prefix, 'data'].join('/'),
   channelsPath: () => [host, prefix, 'channels'].join('/'),
   channelPath: (id) => [host, prefix, 'channels', id].join('/'),
   channelMessagesPath: (id) => [host, prefix, 'channels', id, 'messages'].join('/'),
