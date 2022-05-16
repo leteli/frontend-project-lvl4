7177af058e354f426ae25241995e70f3194fc795
Nastia Kartseva <danetzachem@gmail.com>
2022-05-16 19:38:07 +0300

    add asyncthunk

---
+++ src/slices/fetchData.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import axios from 'axios';
+import { getAuthHeader } from '../contexts/auth.jsx';
+import routes from '../routes.js';
+
+const fetchData = createAsyncThunk(
+  'messages/fetchData',
+  async () => {
+    const { data } = await axios.get(routes.dataPath(), {
+      headers: getAuthHeader(),
+    });
+    return data;
+  },
+);
+
+export default fetchData;
