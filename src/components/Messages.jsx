9e4770610616181bfb55a4762e00cce18898d6f7
Nastia Kartseva <danetzachem@gmail.com>
2022-05-18 19:19:41 +0300

    update message rendering

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import {
-  Col,
-  Button,
-} from 'react-bootstrap';
-import { Formik, Form, Field } from 'formik';
+import { Col } from 'react-bootstrap';
 
 import { selectors as messagesSelectors } from '../slices/messagesSlice.js';
 import { selectors as channelsSelectors } from '../slices/channelsSlice.js';
+
+import MessageForm from './MessageForm.jsx';
 
 const Messages = () => {
   const messages = useSelector(messagesSelectors.selectAll);
   console.log(messages);
   const currentChannel = useSelector((state) => {
     const { currentChannelId } = state.channels;
-    console.log(currentChannelId);
     return channelsSelectors.selectById(state, currentChannelId);
   });
 
@@ -31,39 +28,15 @@ const Messages = () => {
           </span>
         </div>
         <div id="messages-box" className="chat-messages overflow-auto px-5">
-          {messages.length > 0 && messages.map((m) => (
-            <div className="text-break mb-2">
-              <b>{m.author}</b>
-              :
-              {m.text}
+          {messages.length > 0 && messages.map((message) => (
+            <div key={message.id} className="text-break mb-2">
+              <b>{message.username}</b>
+              <span> : </span>
+              {message.body}
             </div>
           ))}
         </div>
-        <div className="mt-auto px-5 py-3">
-          <Formik
-            initialValues={{
-              body: '',
-            }}
-            onSubmit={{}}
-          >
-            <Form className="py-1 border rounded-2">
-              <div className="input-group">
-                <Field
-                  name="body"
-                  className="form-control border-0 p-0 ps-2"
-                  placeholder="Введите сообщение..."
-                  aria-label="Новое сообщение"
-                />
-                <Button type="submit" disabled variant="" className="btn-group-vertical">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                  </svg>
-                  <span className="visually-hidden">Отправить</span>
-                </Button>
-              </div>
-            </Form>
-          </Formik>
-        </div>
+        <MessageForm />
       </div>
     </Col>
   );
