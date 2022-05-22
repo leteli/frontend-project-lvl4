79feba7130b58fb5f59c43e6c210679d41ea53cf
Nastia Kartseva <danetzachem@gmail.com>
2022-05-22 18:05:34 +0300

    update store

--- src/slices/index.js
+++ src/slices/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './channelsSlice.js';
 import messagesReducer from './messagesSlice.js';
 import usersReducer from './usersSlice.js';
+import modalsReducer from './modalsSlice.js';
 
 export default configureStore({
   reducer: {
     channels: channelsReducer,
     messages: messagesReducer,
     users: usersReducer,
+    modals: modalsReducer,
   },
 });
