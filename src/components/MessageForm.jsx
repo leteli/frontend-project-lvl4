bfd79115035465ac0b5be513db0e7586f6cdebc6
Nastia Kartseva <danetzachem@gmail.com>
2022-05-22 18:03:36 +0300

    disable button when sending message

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Button } from 'react-bootstrap';
 
@@ -9,17 +10,22 @@ import { Formik } from 'formik';
 import { sendNewMessage } from '../slices/messagesSlice.js';
 
 const MessageForm = () => {
+  const inputRef = useRef();
+  useEffect(() => inputRef.current.focus());
   const dispatch = useDispatch();
+  const channelId = useSelector((state) => state.channels.currentChannelId);
   return (
     <div className="mt-auto px-5 py-3">
       <Formik
         initialValues={{
           body: '',
         }}
-        onSubmit={(values) => {
+        onSubmit={(values, actions) => {
           const { username } = JSON.parse(localStorage.getItem('userId'));
-          const message = { ...values, username };
+          const message = { ...values, channelId, username };
           dispatch(sendNewMessage(message));
+          values.body = '';
+          actions.setSubmitting(false);
         }}
       >
         {(formik) => (
@@ -29,6 +35,7 @@ const MessageForm = () => {
           >
             <div className="input-group">
               <input
+                ref={inputRef}
                 name="body"
                 type="text"
                 className="form-control border-0 p-0 ps-2"
@@ -41,7 +48,7 @@ const MessageForm = () => {
                 type="submit"
                 variant=""
                 className="btn-group-vertical"
-                disabled={!formik.dirty}
+                disabled={formik.values.body === '' || formik.isSubmitting}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
