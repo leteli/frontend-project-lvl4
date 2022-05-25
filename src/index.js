73484b1826f6a86260b4b7c9bef7f8fe58b2b0ba
Nastia Kartseva <danetzachem@gmail.com>
2022-05-25 23:10:19 +0300

    run the app

--- src/index.js
+++ src/index.js
@@ -3,22 +3,6 @@ import 'core-js/stable/index.js'; // зачем?
 import 'regenerator-runtime/runtime.js'; // зачем?
 
 import '../assets/application.scss'; // зачем?
+import init from './init.js';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-
-import App from './components/App.jsx';
-import store from './slices/index.js';
-
-if (process.env.NODE_ENV !== 'production') {
-  localStorage.debug = 'chat:*';
-}
-
-const root = ReactDOM.createRoot(document.getElementById('chat'));
-
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-);
+init();
