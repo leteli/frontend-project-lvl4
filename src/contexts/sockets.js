f3e0fa8ee41bca369a0785e74d5ec749b83f3103
Nastia Kartseva <danetzachem@gmail.com>
2022-05-26 17:12:36 +0300

    edit error handling

--- src/contexts/sockets.js
+++ src/contexts/sockets.js
@@ -4,48 +4,32 @@ export const socketContext = createContext();
 
 export const buildSocketApi = (socket) => {
   const sendNewMessage = async (newMessage) => {
-    try {
-      await socket.emit('newMessage', newMessage, (response) => {
-        if (response.status !== 'ok') {
-          throw new Error('Network error: message delivery failed');
-        }
-      });
-    } catch (err) {
-      console.log(err.message);
-    }
+    await socket.emit('newMessage', newMessage, (response) => {
+      if (response.status !== 'ok') {
+        throw new Error('Network error: message delivery failed');
+      }
+    });
   };
   const addNewChannel = async (newChannel) => {
-    try {
-      await socket.emit('newChannel', newChannel, (response) => {
-        if (response.status !== 'ok') {
-          throw new Error('Network error: channel adding failed');
-        }
-      });
-    } catch (err) {
-      console.log(err.message);
-    }
+    await socket.emit('newChannel', newChannel, (response) => {
+      if (response.status !== 'ok') {
+        throw new Error('Network error: channel adding failed');
+      }
+    });
   };
   const removeChannel = async (channel) => {
-    try {
-      await socket.emit('removeChannel', channel, (response) => {
-        if (response.status !== 'ok') {
-          throw new Error('Network error: channel removing failed');
-        }
-      });
-    } catch (err) {
-      console.log(err.message);
-    }
+    await socket.emit('removeChannel', channel, (response) => {
+      if (response.status !== 'ok') {
+        throw new Error('Network error: channel removing failed');
+      }
+    });
   };
   const renameChannel = async (channel) => {
-    try {
-      await socket.emit('renameChannel', channel, (response) => {
-        if (response.status !== 'ok') {
-          throw new Error('Network error: channel renaming failed');
-        }
-      });
-    } catch (err) {
-      console.log(err.message);
-    }
+    await socket.emit('renameChannel', channel, (response) => {
+      if (response.status !== 'ok') {
+        throw new Error('Network error: channel renaming failed');
+      }
+    });
   };
 
   return {
