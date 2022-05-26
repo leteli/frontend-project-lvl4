02165a37ccb0aaaec07bca54acdfa8adc7037584
Nastia Kartseva <danetzachem@gmail.com>
2022-05-26 21:08:05 +0300

    move authprovider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ import {
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { AuthProvider } from '../contexts/auth.jsx';
 import ChatNavbar from './ChatNavbar.jsx';
 import LoginForm from './LoginForm.jsx';
 import SignupForm from './SignupForm.jsx';
@@ -17,28 +16,26 @@ import Chat from './Chat.jsx';
 import PrivateRoute from './PrivateRoute.jsx';
 
 const App = () => (
-  <AuthProvider>
-    <Router>
-      <div className="d-flex flex-column h-100">
-        <ChatNavbar />
-        <Switch>
-          <Route path="/login">
-            <LoginForm />
-          </Route>
-          <Route path="/signup">
-            <SignupForm />
-          </Route>
-          <PrivateRoute>
-            <Chat />
-          </PrivateRoute>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-      <ToastContainer />
-    </Router>
-  </AuthProvider>
+  <Router>
+    <div className="d-flex flex-column h-100">
+      <ChatNavbar />
+      <Switch>
+        <Route path="/login">
+          <LoginForm />
+        </Route>
+        <Route path="/signup">
+          <SignupForm />
+        </Route>
+        <PrivateRoute>
+          <Chat />
+        </PrivateRoute>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
+    </div>
+    <ToastContainer />
+  </Router>
 );
 
 export default App;
