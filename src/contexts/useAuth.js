4954939c39abe457fcfdc2cb7fc93b33980c91cc
Nastia Kartseva <danetzachem@gmail.com>
2022-06-07 22:44:47 +0300

    remove custom hooks

--- src/contexts/useAuth.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { authContext } from '../index.js';
-
-const useAuth = () => useContext(authContext);
-
-export default useAuth;
--- src/contexts/useSocket.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { socketContext } from '../index.js';
-
-const useSocket = () => useContext(socketContext);
-
-export default useSocket;
