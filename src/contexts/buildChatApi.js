80786c1cfa462003e8553b8c95b257d24d35fe93
Nastia Kartseva <danetzachem@gmail.com>
2022-06-07 22:45:02 +0300

    remove promise from sockets

--- src/contexts/buildChatApi.js
+++ src/contexts/buildChatApi.js
@@ -1,27 +1,27 @@
 const buildChatApi = (socket) => {
-  const sendNewMessage = async (newMessage) => {
-    await socket.emit('newMessage', newMessage, (response) => {
+  const sendNewMessage = (newMessage) => {
+    socket.emit('newMessage', newMessage, (response) => {
       if (response.status !== 'ok') {
         throw new Error('Network error: message delivery failed');
       }
     });
   };
-  const addNewChannel = async (newChannel) => {
-    await socket.emit('newChannel', newChannel, (response) => {
+  const addNewChannel = (newChannel) => {
+    socket.emit('newChannel', newChannel, (response) => {
       if (response.status !== 'ok') {
         throw new Error('Network error: channel adding failed');
       }
     });
   };
-  const removeChannel = async (channel) => {
-    await socket.emit('removeChannel', channel, (response) => {
+  const removeChannel = (channel) => {
+    socket.emit('removeChannel', channel, (response) => {
       if (response.status !== 'ok') {
         throw new Error('Network error: channel removing failed');
       }
     });
   };
-  const renameChannel = async (channel) => {
-    await socket.emit('renameChannel', channel, (response) => {
+  const renameChannel = (channel) => {
+    socket.emit('renameChannel', channel, (response) => {
       if (response.status !== 'ok') {
         throw new Error('Network error: channel renaming failed');
       }
